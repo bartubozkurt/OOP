6d56ceef0001f63ced40d01ae3145fcb424781a0
Bartu Bozkurt <45657409+bartubozkurt@users.noreply.github.com>
2020-02-26 12:58:49 +0300

    Add files via upload

---
+++ OOPencapsulation/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPEncapsulation
+{
+    class CustomerManager
+    {
+
+        public int Id;
+        public string FirstName;
+        public String LastName;
+        public string City;
+
+        public   void Add()
+        {
+
+             Console.WriteLine("Customer Added!");
+
+        }
+    }
+}
---
+++ OOPencapsulation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customer = new CustomerManager();
+            //customer.Add();
+
+            customer.City = "İzmir";
+            customer.Id = 1;
+            customer.FirstName = "Bartu";
+            customer.LastName = "Bozkurt";
+
+            Console.WriteLine(customer.FirstName);
+            Console.WriteLine(customer.LastName);
+            Console.WriteLine(customer.City);
+
+            Console.ReadLine();
+        }
+
+    }
+}
