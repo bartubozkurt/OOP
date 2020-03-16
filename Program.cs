ad8cdc05ae465f576d8843f11f5b6cfde76c3956
Bartu Bozkurt <45657409+bartubozkurt@users.noreply.github.com>
2020-03-16 22:54:28 +0300

    RecapDemo2

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPRecapDemo2
+{
+    interface ILogger
+    {
+        void log();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.logger = new DatabaseLogger();
+            //customerManager.logger = new FileLogger();
+            //customerManager.logger = new Sms();
+            customerManager.Add();
+            Console.ReadLine();
+       }
+    }
+    class CustomerManager
+    {
+        public ILogger logger{ get; set; }
+
+        public void Add()
+        {
+            logger.log();
+            Console.WriteLine("Customer Added!");
+        }
+    }
+    class DatabaseLogger:ILogger
+    {
+        public void log()
+        {
+            Console.WriteLine("Logged to Database!");
+        }
+    }
+    class FileLogger: ILogger
+    {
+        public void log()
+        {
+            Console.WriteLine("Logged to file!");
+        }
+    }
+    class Sms : ILogger
+    {
+        public void log()
+        {
+            Console.WriteLine("Logged to SMS!");
+        }
+    }
+    
+}
