5ae4372dfff06f5ee44d9e1fd90ffd52defb0148
Bartu Bozkurt <45657409+bartubozkurt@users.noreply.github.com>
2020-04-07 00:13:06 +0300

    Add files via upload

---
+++ File/klasorOlusturma.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasorOlusturma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ogrenci numarasi giriniz : ");
+            string ogrNo = Console.ReadLine();
+            Console.Write("Kayıt Olunacak ders kodu giriniz : ");
+            string dersKodu = Console.ReadLine();
+
+            string klasorYolu = @"c:\test\" + dersKodu;
+
+            if (System.IO.Directory.Exists(klasorYolu) == true)
+            {
+                string ogrKlasorHedefYolu = @"c:\test\" + dersKodu + "\\" + ogrNo;
+                System.IO.Directory.CreateDirectory(ogrKlasorHedefYolu);
+                Console.WriteLine("{0} nolu öğrenci {1} nolu derse kayıt oldu!", ogrNo, dersKodu);
+            }
+            else
+            {
+                Console.Write("Boyle bir ders yoktur!");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ klasorSilme.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasorSilme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string klasorYolu = @"c:\Notlarım\";
+            if(System.IO.Directory.Exists(klasorYolu) == true)
+            {
+                System.IO.Directory.Delete(klasorYolu, true);
+                Console.WriteLine("Klasör tüm içeriğiyle birlikte silindi!");
+            }
+            else
+            {
+                Console.WriteLine("Klasör mevcut değil!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
